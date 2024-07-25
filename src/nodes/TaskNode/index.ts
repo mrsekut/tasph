2fcf45679b122f2c2f2643d705d5c95303d64c7f
mrsekut <k.cloudspider@gmail.com>
2024-07-25 16:26:35 +0900

    fix: taskDataAtomをjotai-opticsを使って構成する
    taskDataAtomを基準にtaskNodeAtomを構成すると問題が起きた
     input要素で連続で更新するとfocusが外れてしまう
    そのため、taskNodeAtomを基準としつつ、jotai-opticsを使ってtaskDataAtomを構成している

--- src/nodes/TaskNode/index.ts
+++ src/nodes/TaskNode/index.ts
@@ -1,28 +1,24 @@
 import type { Node } from "@xyflow/react";
 import { atom } from "jotai";
+import { focusAtom } from "jotai-optics";
 import { atomFamily } from "jotai/utils";
 import type { NodeId } from "../atom";
 
 export type TaskNode = Node<Task>;
 
 export const taskNodeAtom = atomFamily((id: NodeId) =>
-  atom(
-    get => {
-      return {
-        id,
-        type: "task",
-        position: get(positionAtom(id)),
-        data: get(taskDataAtom(id)),
-      } as const satisfies TaskNode;
+  atom<TaskNode>({
+    id,
+    type: "task",
+    position: { x: 0, y: 0 },
+    data: {
+      title: "",
+      lines: [],
+      parents: [],
+      children: [],
     },
-    (_get, set, n: TaskNode) => {
-      set(positionAtom(id), n.position);
-      set(taskDataAtom(id), n.data);
-    },
-  ),
+  }),
 );
-
-const positionAtom = atomFamily((_id: NodeId) => atom({ x: 0, y: 0 }));
 
 export type Task = {
   title: string;
@@ -31,13 +27,14 @@ export type Task = {
   children: NodeId[];
 };
 
-const taskDataAtom = atomFamily((_id: NodeId) =>
-  atom<Task>({
-    title: "",
-    lines: [],
-    parents: [],
-    children: [],
-  }),
+/**
+ * NOTE:
+ * - taskDataAtomを基準にtaskNodeAtomを構成すると問題が起きた
+ *   - input要素で連続で更新するとfocusが外れてしまう
+ *   - そのため、taskNodeAtomを基準としつつ、jotai-opticsを使ってtaskDataAtomを構成している
+ */
+const taskDataAtom = atomFamily((id: NodeId) =>
+  focusAtom(taskNodeAtom(id), o => o.prop("data")),
 );
 
 export const setTitleAtom = atomFamily((id: NodeId) =>
