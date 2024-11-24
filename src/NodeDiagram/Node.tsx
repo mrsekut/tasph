7d0efe1d148a73a2a83f978a1ee7948da26cc468
mrsekut <k.cloudspider@gmail.com>
2024-11-24 20:11:45 +0900

    fix: はみ出るので高さを指定しない

--- src/NodeDiagram/Node.tsx
+++ src/NodeDiagram/Node.tsx
@@ -8,7 +8,7 @@ type Props = {
 // TODO: style
 export const Node: React.FC<Props> = ({ node }) => {
   return (
-    <div className="w-32 h-12 p-2 grid justify-center items-center rounded-lg bg-blue-300">
+    <div className="w-32 p-2 grid justify-center items-center rounded-lg bg-blue-300">
       {node.id}
     </div>
   );
